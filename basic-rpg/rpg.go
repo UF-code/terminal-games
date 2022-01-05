9ddf900e3d798e7d1a59af0d2ddad37062450d4a
uf-code <ugur.firat.developer@gmail.com>
2022-01-05 19:44:42 +0300

    creating npc's, player, enemy

--- basic-rpg/rpg.go
+++ basic-rpg/rpg.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+type Character struct {
+	Name   string
+	Class  string
+	Health int
+	Power  int
+}
+
+type NPC_Shop struct {
+	Character
+}
+
+type NPC_Quest struct {
+	Character
+}
+
+type Enemy struct {
+	Character
+}
+
+type Player struct {
+	Character
+}
 
 func main() {
-	fmt.Println("Hey")
+
 }
