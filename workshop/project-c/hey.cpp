ff87844dfb8870fc815254dfc645aedb5c053f63
uf-code <ugur.firat.developer@gmail.com>
2022-01-14 18:25:30 +0300

    running c/c++

---
+++ workshop/project-c/hey.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World ..." << endl;
+
+    cout << "Give me a cookie or I will eat you" << endl;
+
+    return 0;
+}
