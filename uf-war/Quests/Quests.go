2f561b97708364d343cfe85ef5f4cc90e8657fe4
uf-code <ugur.firat.developer@gmail.com>
2022-01-15 17:20:32 +0300

    adding quest templates

--- uf-war/Quests/Quests.go
+++ uf-war/Quests/Quests.go
@@ -1,1 +1,59 @@
 package Quests
+
+type Quests struct {
+	Level0
+	Level1
+	Level2
+	Level3
+	Level4
+	Level5
+	Level6
+	Level7
+	Level8
+	Level9
+}
+
+type Reward struct {
+	Exp  int
+	Gold int
+}
+type Level0 struct {
+	Level int
+	Reward
+}
+type Level1 struct {
+	Level int
+	Reward
+}
+type Level2 struct {
+	Level int
+	Reward
+}
+type Level3 struct {
+	Level int
+	Reward
+}
+type Level4 struct {
+	Level int
+	Reward
+}
+type Level5 struct {
+	Level int
+	Reward
+}
+type Level6 struct {
+	Level int
+	Reward
+}
+type Level7 struct {
+	Level int
+	Reward
+}
+type Level8 struct {
+	Level int
+	Reward
+}
+type Level9 struct {
+	Level int
+	Reward
+}
