b8a21d67945a7e0aefab465572318c05744f05d0
uf-code <ugur.firat.developer@gmail.com>
2022-01-15 17:12:33 +0300

    completing weapons and adding clothes templates

--- uf-war/Items/Items.go
+++ uf-war/Items/Items.go
@@ -3,6 +3,7 @@ package Items
 type Items struct {
 	Potion
 	Weapons
+	Armors
 }
 
 type Potion struct {
@@ -19,6 +20,12 @@ type HealthPotion struct {
 }
 
 type Weapons struct {
+	Sword
+	Axe
+	Dagger
+	Bow
+	Wand
+	Staff
 }
 
 type Sword struct {
@@ -39,4 +46,54 @@ type Dagger struct {
 type Bow struct {
 	Class string
 	Power int
+}
+
+type Wand struct {
+	Class string
+	Power int
+}
+
+type Staff struct {
+	Class string
+	Power int
+}
+
+type Armors struct {
+	Plate
+	IronPlate
+	Leather
+	Chitin
+	Robe
+	SilkRobe
+}
+
+type Plate struct {
+	Class  string
+	Level  int
+	Health int
+}
+type IronPlate struct {
+	Class  string
+	Level  int
+	Health int
+}
+type Leather struct {
+	Class  string
+	Level  int
+	Health int
+}
+type Chitin struct {
+	Class  string
+	Level  int
+	Health int
+}
+type Robe struct {
+	Class  string
+	Level  int
+	Health int
+}
+type SilkRobe struct {
+	Class  string
+	Level  int
+	Health int
 }
